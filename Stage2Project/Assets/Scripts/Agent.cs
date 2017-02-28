8d01935ff81f5be54ed575c599e92b664833c20d
Matthew Duddington <math.duddington@gmail.com>
2017-02-28 19:39:02 +0000

    Hunt player script, based on raycast check for LoS and memory of location seen

--- Stage2Project/Assets/Scripts/Agent.cs
+++ Stage2Project/Assets/Scripts/Agent.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Agent : MonoBehaviour
 { 
+    public enum BehaviourState { Disabled, Wandering, Hunting }
+    private BehaviourState mState;
+
     static public int ActiveAgentsCount { get; private set; }
 
     static public float ColliderRadius { get; private set; }
@@ -13,15 +16,24 @@ public class Agent : MonoBehaviour
 
     static private GameObject agentPrefab;  // Remove once randomised function written
 
+    public RaycastHit[] hits { get; private set; }
+
     private Vector3 gravity;
 
-    public Vector3 FlockVector;  //{ get; set; }
-    public Vector3 HuntVector;   //{ get; set; }
-    public Vector3 WanderVector; //{ get; set; }
-    public Vector3 locomoationForce;  // TODO set back to private
+    public Vector3 FlockForce;  //{ get; set; }
+    public Vector3 WanderForce; //{ get; set; }
+    public Vector3 AvoidWallsForce; //{ get; set; }
+    public Vector3 HuntForce;   //{ get; set; }
+
+    private HuntPlayer mHunt;
+    private FlockWithGroup mFlock;
+    private Wander mWander;
+    private AvoidWalls mAvoid;
 
     [SerializeField]
-    private float Speed = 500;
+    private float MaxSpeed = 400;
+    [SerializeField]
+    private float TurningSpeed = 50;
 
     private Rigidbody mBody;
 
@@ -39,9 +51,16 @@ public class Agent : MonoBehaviour
     {
         mBody = gameObject.GetComponent<Rigidbody>();
         ColliderRadius = GetComponent<CapsuleCollider>().radius;
+        hits = new RaycastHit[16];
+
+        mHunt = GetComponent<HuntPlayer>();
+        mFlock = GetComponent<FlockWithGroup>();
+        mWander = GetComponent<Wander>();
+        mAvoid = GetComponent<AvoidWalls>();
 
         ActiveAgentsCount++;
         Reset();
+//        StartCoroutine(Co_DecisionTick());  // TODO Remove
     }
 
     void Start()
@@ -51,41 +70,84 @@ public class Agent : MonoBehaviour
 
     public void Reset()
     {
-        gameObject.SetActive(false);
-        ActiveAgentsCount--;
-        transform.position = Vector3.down * 100;
-        FlockVector = Vector3.zero;
-        HuntVector = Vector3.zero;
-        locomoationForce = Vector3.zero;
+        gameObject.SetActive(false);              // Disable components
+        ActiveAgentsCount--;                      // Remove from count of agents in play
+        transform.position = Vector3.down * 100;  // Store agent outside play area
+
+        // Clear steering force components
+        FlockForce = Vector3.zero;
+        WanderForce = Vector3.zero;
+        AvoidWallsForce = Vector3.zero;
+        HuntForce = Vector3.zero;
+
+        mState = BehaviourState.Disabled; // Put behaviour machine in hibernation
     }
 
     public void Init()
     {
-        gameObject.SetActive(true);
-        ActiveAgentsCount++;
-        transform.position = Map.Get.AgentSpawnLocation;
-        transform.rotation = Quaternion.identity;
-        StartCoroutine(Co_DesisionTick());
+        gameObject.SetActive(true);  // Enable components
+        ActiveAgentsCount++;         // Add to count of agents in play
+
+        // Place agent on spawn point
+        mBody.MovePosition(Map.Get.AgentSpawnLocation);
+        mBody.MoveRotation(Quaternion.identity);
+
+        mState = BehaviourState.Wandering;  // Set agent to wander state
+
+        StartCoroutine(Co_DecisionTick());  // Run behaviour machine
     }
 
-    private IEnumerator Co_DesisionTick()
+    // Coroutiene behaviour machine, handling global awareness and state switching
+    private IEnumerator Co_DecisionTick()
     {
-        while (gameObject.activeSelf)
+        while (mState != BehaviourState.Disabled)
         {
-            // Use context map to decide on weighting of heading
-            locomoationForce = Vector3.zero;
-           // locomoationForce += FlockVector + HuntVector;
-            locomoationForce += WanderVector;
-            locomoationForce *= mBody.mass;
-            locomoationForce *= Speed * Time.fixedDeltaTime;
+            for (int i = 0; i < 16; i++)
+            {
+                Vector3 rayDirection = new Vector3(Mathf.Cos(22.5f * i), 0, Mathf.Sin(22.5f * i));
+                Physics.Raycast(transform.position + Vector3.up, rayDirection, out hits[i]);
+                Debug.DrawRay(transform.position + Vector3.up, rayDirection, Color.blue, desisionTickTime);
+            }
+
+            if (HuntForce.sqrMagnitude > 0)
+            {
+                mState = BehaviourState.Hunting;
+            }
+            else
+            {
+                mState = BehaviourState.Wandering;
+            }
+
             yield return waitForDesisionTick;
         }
     }
 
     void FixedUpdate()
     {
-        mBody.AddForce(locomoationForce + gravity);
-//        mBody.velocity = new Vector3(mBody.velocity.x, Mathf.Clamp(mBody.velocity.y, -9.8f, 9.8f), mBody.velocity.z);
+        // Add forces according to desision state
+        Vector3 totalForce = Vector3.zero;
+
+        mHunt.UpdateBehaviour();  // Check if player has been seen and calculate steering force contribution
+        totalForce += HuntForce;
+
+        if (mState == BehaviourState.Wandering)
+        {
+            totalForce += FlockForce;
+
+            totalForce += WanderForce;
+
+            mAvoid.UpdateBehaviour();
+            totalForce += AvoidWallsForce;
+
+        }
+
+//        totalForce /= mBody.mass;
+        Vector3 steeringForce = totalForce - new Vector3(mBody.velocity.x, 0, mBody.velocity.z);
+
+//        steeringForce = Vector3.ClampMagnitude(steeringForce, MaxSpeed);
+
+        mBody.velocity = mBody.velocity + steeringForce + (gravity * Time.fixedDeltaTime);
+        //mBody.AddForce(gravity);
 
         velocityV = mBody.velocity;
     }
---
+++ Stage2Project/Assets/Scripts/HuntPlayer.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HuntPlayer : MonoBehaviour {
+
+    [SerializeField]
+    private float HuntStrength = 100;
+    
+    private Agent mAgent;
+
+    public float PlayerSightingDecayTime = 4;
+    private float PlayerSightingTime;
+
+    public bool HasSeenPlayer { get; private set; }
+
+    private Vector3 lastKnownLocation;
+
+    void Awake()
+    {
+        mAgent = GetComponent<Agent>();
+
+        HasSeenPlayer = false;
+    }
+
+    void Start()
+    {
+
+    }
+
+    public void UpdateBehaviour()
+    {
+        foreach(RaycastHit hit in mAgent.hits)
+        {
+            if (hit.collider != null)
+            {
+                if (hit.collider.CompareTag("Player"))
+                {
+                    HasSeenPlayer = true;
+                    PlayerSightingTime = Time.fixedTime + PlayerSightingDecayTime;
+                    lastKnownLocation = hit.collider.transform.position;
+                }
+            }
+        }
+        if (Time.fixedTime > PlayerSightingTime)
+        {
+            HasSeenPlayer = false;
+        }
+
+        if (HasSeenPlayer)
+        {
+            mAgent.HuntForce = (-transform.position + lastKnownLocation).normalized * HuntStrength;
+        }
+        else
+        {
+            mAgent.HuntForce = Vector3.zero;
+        }
+    }
+}
